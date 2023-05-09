a374c6af9e6bfbfb17127000766fc1c97170a013
seba-wetzel <seba.wetzel@gmail.com>
2023-05-08 23:03:04 -0300

    Add: pinia para la gestion global de sedes seleccionadas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
 import "./style.css";
-
 import router from "src/Routes/index.js";
+import { createPinia } from "pinia";
 import App from "src/App.vue";
+
 const app = createApp(App);
+const pinia = createPinia();
 app.use(router);
+app.use(pinia);
+
 app.mount("#app");
