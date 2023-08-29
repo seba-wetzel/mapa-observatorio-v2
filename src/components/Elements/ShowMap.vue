3944debfc8196224112b22b7e2ed43ea0b5f78a8
seba-wetzel <seba.wetzel@gmail.com>
2023-08-29 13:38:49 -0300

    Fix: el boton mostrar mapa solo funcionaba en el menu de opciones

--- src/components/Elements/ShowMap.vue
+++ src/components/Elements/ShowMap.vue
@@ -2,10 +2,16 @@
 import { MapIcon } from "@heroicons/vue/24/outline";
 import { useMenu } from "src/composables/useMenu";
 const { close: showMap } = useMenu("options");
+const { close: closeSearch } = useMenu("search");
+
+const close = () => {
+  closeSearch();
+  showMap();
+};
 </script>
 <template>
   <button
-    @click="showMap"
+    @click="close"
     class="flex flex-row flex-grow justify-center font-bold text-blue-500 m-2"
   >
     <MapIcon class="inline w-6 h-6" /> Mostrar mapa
